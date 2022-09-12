8125b8b52d03e76af348e1e82296a2b169cecd55
Lucas Palharini <lucas230991@hotmail.com>
2022-09-12 12:00:59 -0300

    build: configuring the api base

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+//import ##### from './#####';
+
+const router = Router();
+
+//router.use(#####);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+//Libs
+import express, { json } from 'express';
+import cors from "cors";
+import 'dotenv/config';
+import 'express-async-errors';
+
+//Main Route
+import router from './routes/index';
+
+//Error Handler
+//import { errorHandler } from './middlewares/errorHandler.middleware.js';
+
+//Configs
+const server = express();
+
+server.use(cors());
+server.use(json());
+
+server.use(router);
+
+//server.use(errorHandler)
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor tá rodando mais que a bola quadrada do Quico, na porta ${PORT}.`);
+});
