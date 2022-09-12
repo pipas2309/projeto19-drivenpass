7228163becdf41ae7c268a3ad764e5269604c1be
Lucas Palharini <lucas230991@hotmail.com>
2022-09-12 15:14:07 -0300

    feat: random name generator created

---
+++ src/utils/genereteName.util.ts
@@ -0,0 +1,11 @@
+export function randonMemeName() {
+    const firstPart = ["Seua, ", "Done, ", "Ao, ", "Menine, ", "Moçe, ", "Ei ", '', '', ''];
+    const midPart = ["Nel", "Adamas", "Hele", "Petru", "Dindi", "Lu", "Ca", "Fa", "Bi", "Zé", "Nhô", "Nhâ"];
+    const lastPart = ["son", "sona", "no", "tora", "tor", "nho", "son", "nha", "na", "tus", "kio", "kia", ""];
+
+    const randomFirst = Math.floor(Math.random() * firstPart.length);
+    const randomMid = Math.floor(Math.random() * midPart.length);
+    const randomLast = Math.floor(Math.random() * lastPart.length);
+
+    return firstPart[randomFirst] + midPart[randomMid] + lastPart[randomLast]
+}
